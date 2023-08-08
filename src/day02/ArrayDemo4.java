7ea187e045701a7525ac57f687ed1a9147e2c328
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-08 15:08:49 +0800

    java

--- src/day02/ArrayDemo4.java
+++ src/day02/ArrayDemo4.java
@@ -14,7 +14,9 @@ package day02;
 public class ArrayDemo4 {
 
 	public static void main(String[] args) {
-		
+		double[] stocks = {7.6, 3.9, 15.6, 28.3, 1.2, 10.8, 35.3, 45.6, 10.2, 0.5};
+        double[] bonds  = {6.8, 7.2, 6.8, 7.5, 6.9, 7.9, 7.9, 7.1, 7.2};
+
 
 	}
 
