0753b18e57e5a941646c0bc00c70c9acb59e9ac9
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-08 15:53:35 +0800

    java

---
+++ src/day02/ArrayDemo5.java
@@ -0,0 +1,17 @@
+package day02;
+
+/*
+ * 調查五位學生之身高及體重如下:
+ * 身高：172、168、164、170、176(公分)
+ * 體重：62、57、58、64、64(公斤)
+ * 求身高,體重分散程度哪一個大
+ * */
+
+public class ArrayDemo5 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
