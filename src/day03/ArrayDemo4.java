75c1677147de4ef35a98e37a60ad7cbc4471fa57
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 11:40:49 +0800

    java

--- src/day03/ArrayDemo4.java
+++ src/day03/ArrayDemo4.java
@@ -4,10 +4,25 @@ public class ArrayDemo4 { // begin
 	
 	public static void main(String[] args) { // main-begin
 		int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-		// 1.請印出偶數 2.求出總和
+		// 1.請印出偶數 2.印出所有偶數的總和
 		// 使用 for-loop, for-each 與 Stream 的 forEach
+		int sum1 = 0; // 所有偶數的總和
+		for(int i=0;i<values.length;i++) {
+			if(values[i] % 2 == 0) {
+				System.out.println(values[i]);
+				sum1 += values[i]; // sum1 = sum1 + values[i];
+			}
+		}
+		System.out.printf("總和:%d\n", sum1);
 		
-		
+		int sum2 = 0;
+		for(int x : values) {
+			if(x % 2 == 0) {
+				System.out.println(x);
+				sum2 += x;
+			}
+		}
+		System.out.printf("總和:%d\n", sum2);
 		
 	} // main-end
 	
