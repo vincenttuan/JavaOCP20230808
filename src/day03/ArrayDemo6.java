f15b1293551dd27f72d8811f446c0e9da5c03b93
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 13:19:29 +0800

    java

--- src/day03/ArrayDemo6.java
+++ src/day03/ArrayDemo6.java
@@ -9,9 +9,9 @@ public class ArrayDemo6 {
 		// 取得最高分, 最低分, 總分, 平均, 個數
 		int maxScore = Arrays.stream(scores).max().getAsInt(); // 最高分
 		int minScore = Arrays.stream(scores).min().getAsInt(); // 最低分
-		int sum = Arrays.stream(scores).sum(); // 總分
-		double avg = Arrays.stream(scores).average().getAsDouble(); // 平均
-		long count = Arrays.stream(scores).count(); // 個數
+		int sum      = Arrays.stream(scores).sum(); // 總分
+		double avg   = Arrays.stream(scores).average().getAsDouble(); // 平均
+		long count   = Arrays.stream(scores).count(); // 個數
 		
 		System.out.printf("最高分 %d, 最低分 %d, 總分 %d, 平均 %.1f, 個數 %d\n",
 				maxScore, minScore, sum, avg, count);
