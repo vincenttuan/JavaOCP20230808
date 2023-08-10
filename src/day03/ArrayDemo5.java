43a4836d6ea1c9604f64cda0402fca7d2711106a
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 11:56:27 +0800

    java

--- src/day03/ArrayDemo5.java
+++ src/day03/ArrayDemo5.java
@@ -1,10 +1,20 @@
 package day03;
 
+import java.util.Arrays;
+
 public class ArrayDemo5 {
 	public static void main(String[] args) {
 		int[] scores = {40, 90, 70, 100, 60};
 		// 請印出最高分 = ?
+		int maxScore = 0; // 最高分
+		for(int i=0;i<scores.length;i++) {
+			if(scores[i] > maxScore) {
+				maxScore = scores[i];
+			}
+		}
+		System.out.printf("最高分 = %d\n", maxScore);
 		
-		
+		int maxScore2 = Arrays.stream(scores).max().getAsInt();
+		System.out.printf("最高分 = %d\n", maxScore2);
 	}
 }
