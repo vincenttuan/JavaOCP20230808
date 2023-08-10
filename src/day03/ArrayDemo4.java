e81cc79590fca392aaf4296c9a7156285cc2451b
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 11:29:53 +0800

    java

---
+++ src/day03/ArrayDemo4.java
@@ -0,0 +1,14 @@
+package day03;
+
+public class ArrayDemo4 { // begin
+	
+	public static void main(String[] args) { // main-begin
+		int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+		// 1.請印出偶數 2.求出總和
+		// 使用 for-loop, for-each 與 Stream 的 forEach
+		
+		
+		
+	} // main-end
+	
+} // end
