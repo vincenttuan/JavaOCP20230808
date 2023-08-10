f82ae2ecd24faa0fa82ec15cb4df46dcb57dc889
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 10:24:52 +0800

    java

---
+++ src/day03/ArrayDemo3.java
@@ -0,0 +1,11 @@
+package day03;
+
+public class ArrayDemo3 {
+	public static void main(String[] args) {
+		// 合法的分數是 0~100
+		// 請印出合法的及格分數
+		int[] scores = new int[]{100, 40, 80, 50, -60, 250, 70};
+		
+		
+	}
+}
