ea08d231e9b512ab8fd2575452fe54fe73bf9387
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 14:25:01 +0800

    java

---
+++ src/day04/StringArray1.java
@@ -0,0 +1,13 @@
+package day04;
+
+import java.util.Arrays;
+
+public class StringArray1 {
+	public static void main(String[] args) {
+		String[] names = {"Vincent", "Anita", "Helen", "Jack", "Jo", "Rose", "Bob"};
+		// 直接印出陣列內容 Arrays.toString(names)
+		System.out.println(Arrays.toString(names));
+		// 請印出每一個名字的長度
+		
+	}
+}
