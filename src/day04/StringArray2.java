2d5faa79c56197c1c4cd57b3b76c232aa8b51aa9
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 15:11:27 +0800

    java

---
+++ src/day04/StringArray2.java
@@ -0,0 +1,9 @@
+package day04;
+
+public class StringArray2 {
+	public static void main(String[] args) {
+		String[] names = {"Vincent", "Anita", "Helen", "Jack", "Jo", "Rose", "Bob"};
+		// 請問平均有幾個字 ? 最長與最短的名字各為何 ?
+		
+	}
+}
