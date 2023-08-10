757269fce7a19db70b8b3c869a9b92af4d4e0e14
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 15:50:22 +0800

    java

---
+++ src/day04/StringArray3.java
@@ -0,0 +1,16 @@
+package day04;
+
+public class StringArray3 {
+	public static void main(String[] args) {
+		// apple 買了 10 個
+		// banana 買了 20 個
+		// orange 買了 30 個
+		// apple 買了 40 個
+		String[] fruits  = {"apple", "banana", "orange", "apple"};
+		int[]    amounts = {10, 20, 30, 40};
+		
+		// 問總共買了幾個 apple ?
+		
+		
+	}
+}
