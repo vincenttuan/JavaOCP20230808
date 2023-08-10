f6206c3528fa855bf1bdc02808c8beaca19e6f1d
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 10:08:41 +0800

    java

---
+++ src/day03/ArrayDemo2.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class ArrayDemo2 {
+
+	public static void main(String[] args) {
+		// 有一個半徑陣列裡面有3個半徑如下
+		int[] radius = {10, 25, 45};
+		// 請透過 for-loop, for-each, Stream 的 forEach
+		// 分別印出這三個半徑的直徑內容
+		
+		
+		
+	}
+
+}
