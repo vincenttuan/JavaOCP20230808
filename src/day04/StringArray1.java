19565673a95481f8c2038f4d696f0cfa063994db
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 14:32:43 +0800

    java

--- src/day04/StringArray1.java
+++ src/day04/StringArray1.java
@@ -8,12 +8,17 @@ public class StringArray1 {
 		// 直接印出陣列內容 Arrays.toString(names)
 		System.out.println(Arrays.toString(names));
 		// 請印出每一個名字的長度
+		// 使用 for-loop
 		for(int i=0;i<names.length;i++) {
 			System.out.println(names[i].length());
 		}
 		
+		// 使用 for-each
 		for(String x : names) {
 			System.out.println(x.length());
 		}
+		
+		
+		
 	}
 }
