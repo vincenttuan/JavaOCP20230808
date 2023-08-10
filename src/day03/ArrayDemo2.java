fd3a757a8160a613a227386e056ec5b13c7bda0e
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 10:22:02 +0800

    java

--- src/day03/ArrayDemo2.java
+++ src/day03/ArrayDemo2.java
@@ -1,5 +1,7 @@
 package day03;
 
+import java.util.Arrays;
+
 public class ArrayDemo2 {
 
 	public static void main(String[] args) {
@@ -7,8 +9,15 @@ public class ArrayDemo2 {
 		int[] radius = {10, 25, 45};
 		// 請透過 for-loop, for-each, Stream 的 forEach
 		// 分別印出這三個半徑的直徑內容
+		for(int i=0;i<radius.length;i++) {
+			System.out.println(radius[i] * 2);
+		}
 		
+		for(int r : radius) {
+			System.out.println(r * 2);
+		}
 		
+		Arrays.stream(radius).forEach(r -> System.out.println(r*2));
 		
 	}
 
