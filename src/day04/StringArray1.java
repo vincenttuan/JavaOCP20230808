3e26d9c385ee721547cbabbb28b37ec40b31fb7d
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-10 14:29:11 +0800

    java

--- src/day04/StringArray1.java
+++ src/day04/StringArray1.java
@@ -8,6 +8,8 @@ public class StringArray1 {
 		// 直接印出陣列內容 Arrays.toString(names)
 		System.out.println(Arrays.toString(names));
 		// 請印出每一個名字的長度
-		
+		for(int i=0;i<names.length;i++) {
+			System.out.println(names[i].length());
+		}
 	}
 }
