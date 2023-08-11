3f99fb04d0b184e0ce098a95a7c42508d49391f6
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-11 13:48:41 +0800

    java

--- src/day06/MultiArray1.java
+++ src/day06/MultiArray1.java
@@ -1,9 +1,11 @@
 package day06;
 
+import java.util.Arrays;
+
 public class MultiArray1 {
 
 	public static void main(String[] args) {
-		int[][] investory = { // 原有庫存
+		int[][] investories = { // 原有庫存
 				{1001, 25}, // 商品編號 1001, 庫存量 25
 				{1002, 30}, // 商品編號 1002, 庫存量 30
 				{1003, 15}  // 商品編號 1003, 庫存量 15
@@ -15,8 +17,16 @@ public class MultiArray1 {
 				{1001, 8}   // 商品編號 1001, 銷售量 8
 		};
 		
+		printInvestory(investories);
 		
-		
+	}
+	
+	// 印出庫存資料
+	public static void printInvestory(int[][] investories) {
+		System.out.println("目前庫存資料:");
+		for(int[] inv : investories) {
+			System.out.println(Arrays.toString(inv));
+		}
 	}
 
 }
