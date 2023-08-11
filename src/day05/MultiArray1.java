ebcbdebbb357349b149b756734fa0024c5cbfcee
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-11 09:25:28 +0800

    java

---
+++ src/day05/MultiArray1.java
@@ -0,0 +1,20 @@
+package day05;
+
+public class MultiArray1 {
+
+	public static void main(String[] args) {
+		// 多陣列運算
+		int[] scoresA = {100, 90, 80}; // A 班成績
+		int[] scoresB = {95, 85, 75};  // B 班成績
+		int[] scoresC = {75, 55, 100}; // C 班成績
+		// 1. 試問 A班+B班+C班 的總分為多少
+		
+		
+		
+		// 2. 試問 A班+B班+C班 的平均為多少
+		
+		
+
+	}
+
+}
