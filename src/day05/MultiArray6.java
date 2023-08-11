dfb568b162b718ec13e701718c1ee5c824eaeefc
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-11 11:28:07 +0800

    java

--- src/day05/MultiArray6.java
+++ src/day05/MultiArray6.java
@@ -4,7 +4,18 @@ public class MultiArray6 {
 	public static void main(String[] args) {
 		int[][] allScores = {{85, 90, 76}, {70, 82}, {65, 67, 100}};
 		// 請求總分與平均
-		
-		
+		int sum = 0;
+		int count = 0;
+		//-------------------------------------
+		for(int[] scores : allScores) {
+			for(int x : scores) {
+				//System.out.println(x);
+				sum += x;
+				count++;
+			}
+		}
+		//-------------------------------------
+		double avg = (double)sum / count;
+		System.out.printf("總分 %d 平均 %.1f\n", sum, avg);
 	}
 }
