9c66a5a984764072835c89b59c9e78f373b25841
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-11 10:27:31 +0800

    java

---
+++ src/day05/MultiArray4.java
@@ -0,0 +1,28 @@
+package day05;
+
+public class MultiArray4 {
+	public static void main(String[] args) {
+		int[][] allScores = {{100, 90, 80}, {70, 60, 50}};
+		// 請印出 allScores 裡面所有的值
+		System.out.println(allScores[0][0]);
+		System.out.println(allScores[0][1]);
+		System.out.println(allScores[0][2]);
+		
+		System.out.println(allScores[1][0]);
+		System.out.println(allScores[1][1]);
+		System.out.println(allScores[1][2]);
+		
+		for(int i=0;i<2;i++) {
+			for(int k=0;k<3;k++) {
+				System.out.println(allScores[i][k]);
+			}
+		}
+		
+		for(int i=0;i<allScores.length;i++) {
+			for(int k=0;k<allScores[i].length;k++) {
+				System.out.println(allScores[i][k]);
+			}
+		}
+		
+	}
+}
