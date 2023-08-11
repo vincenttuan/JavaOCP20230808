6e0e77a58ffebf31d65e879c1fe388c158403731
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-11 10:37:21 +0800

    java

--- src/day05/MultiArray5.java
+++ src/day05/MultiArray5.java
@@ -5,7 +5,16 @@ public class MultiArray5 {
 	public static void main(String[] args) {
 		int[][] allScores = {{100, 90, 80}, {70, 60, 50}};
 		// 請印出 allScores 裡面所有的值
-
+		// for-each
+		// 求總分 = ?
+		int sum = 0;
+		for(int[] scores :allScores) {
+			for(int x : scores) {
+				System.out.println(x);
+				sum += x;
+			}
+		}
+		System.out.println(sum);
 	}
 
 }
