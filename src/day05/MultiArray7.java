947a193f46ab391aa06ccbc9df3cf7be7986f446
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-11 11:48:51 +0800

    java

--- src/day05/MultiArray7.java
+++ src/day05/MultiArray7.java
@@ -9,7 +9,15 @@ public class MultiArray7 {
 								{60.0, 75.5, 55.0}     // 體重
 							};
 		// 請計算這三位的 BMI
-		
+		for(int i=0;i<values[0].length;i++) {
+			//System.out.println("i: " + i);
+			//System.out.println("身高:" + values[0][i]);
+			//System.out.println("體重:" + values[1][i]);
+			double h = values[0][i];
+			double w = values[1][i];
+			double bmi = w / Math.pow(h/100, 2);
+			System.out.println("BMI: " + bmi);
+		}
 
 	}
 
