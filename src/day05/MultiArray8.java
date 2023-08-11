51fcdbe8ee9ad1a203216a2d89725146689038c5
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-11 13:18:59 +0800

    java

--- src/day05/MultiArray8.java
+++ src/day05/MultiArray8.java
@@ -7,9 +7,18 @@ public class MultiArray8 {
 		double[][] values = {
 				{170.0, 60.0}, 
 				{180.0, 75.5}, 
-				{65.0, 55.0}
+				{165.0, 55.0}
 			};
+		
 		// 請計算這三位的 BMI
+		for(double[] value : values) {
+			//System.out.println("身高:" + value[0]);
+			//System.out.println("體重:" + value[1]);
+			double h = value[0];
+			double w = value[1];
+			double bmi = w / Math.pow(h/100, 2);
+			System.out.println(bmi);
+		}
 		
 	}
 
