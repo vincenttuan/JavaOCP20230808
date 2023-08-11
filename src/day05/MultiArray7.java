de3f8e203301c566e63c48521d5ed7bea74b0dc6
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-11 11:33:56 +0800

    java

---
+++ src/day05/MultiArray7.java
@@ -0,0 +1,16 @@
+package day05;
+
+public class MultiArray7 {
+
+	public static void main(String[] args) {
+		// 三人的身高與體重如下
+		double[][] values = {
+								{170.0, 180.0, 165.0}, // 身高
+								{60.0, 75.5, 55.0}     // 體重
+							};
+		// 請計算這三位的 BMI
+		
+
+	}
+
+}
