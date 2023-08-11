ad648d70f7e59c06a3294b22f46795b063d6bb84
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-11 14:33:24 +0800

    java

---
+++ src/day06/MethodDemo4.java
@@ -0,0 +1,30 @@
+package day06;
+
+public class MethodDemo4 {
+
+	public static void main(String[] args) {
+		int[] array = {100, 80, 70};
+		calcSumAndPrint(array); // 計算總和並印出
+		
+		int a = 100;
+		printData(a);
+		
+		String s1 = new String("Java");
+		printString(s1);
+	}
+	
+	public static void printString(String s2) {
+		
+	}
+	
+	
+	public static void printData(int b) {
+		
+	}
+	
+	
+	public static void calcSumAndPrint(int[] x) {
+		
+	}
+
+}
