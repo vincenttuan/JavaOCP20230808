6c58729b2f60398063663abc566ae7b8fce708b0
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-11 13:58:17 +0800

    java

---
+++ src/day06/MethodDemo.java
@@ -0,0 +1,20 @@
+package day06;
+
+public class MethodDemo {
+
+	public static void main(String[] args) {
+		// 印出 Welcome 
+		printWelcome();
+		printWelcome();
+		printWelcome();
+		
+	}
+	
+	public static void printWelcome() {
+		System.out.println("Welcome1");
+		System.out.println("Welcome2");
+		System.out.println("Welcome3");
+	}
+	
+
+}
