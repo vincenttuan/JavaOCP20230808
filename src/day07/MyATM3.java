6c7216b21552de5426a04a972886e46108991b71
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-14 13:15:24 +0800

    java

---
+++ src/day07/MyATM3.java
@@ -0,0 +1,41 @@
+package day07;
+
+public class MyATM3 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+	
+	// 密碼驗證
+	public static void checkPin() {
+		
+	}
+	
+	// 顯示主畫面選單
+	public static void showMenu() {
+		
+	}
+	
+	// 餘額查詢
+	public static void checkBalance() {
+		
+	}
+	
+	// 提款
+	public static void withdraw() {
+		
+	}
+	
+	// 存款
+	public static void deposit() {
+		
+	}
+	
+	// 離開系統
+	public static void quit() {
+		
+	}
+	
+}
