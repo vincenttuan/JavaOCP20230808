3960eb0da2525dd48c3a16df9859f3e00dc6c4f2
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-14 15:27:09 +0800

    java

--- src/day08/Cake.java
+++ src/day08/Cake.java
@@ -2,12 +2,34 @@ package day08;
 
 // 蛋糕物件設計圖
 public class Cake {
-	String name; // 名稱 (至少要2個字)
-	int price;   // 價格 (10 ~ 1000)
-	double sweetness; // 甜度 (0 ~ 100)
+	private String name; // 名稱 (至少要2個字)
+	private int price;   // 價格 (10 ~ 1000)
+	private double sweetness; // 甜度 (0 ~ 100)
 	
 	Cake() {
 		
+	}
+	
+	// 設定名稱的方法
+	void setName(String name) {
+		if(name != null && name.length() >= 2) {
+			this.name = name;
+		}
+	}
+	
+	// 設定價格的方法
+	void setPrice(int price) {
+		if(price >= 10 && price <= 1000) {
+			this.price = price;
+		}
+		
+	}
+	
+	// 設定甜度的方法
+	void setSweetness(double sweetness) {
+		if(sweetness >= 0 && sweetness <= 100) {
+			this.sweetness = sweetness;
+		}
 	}
 	
 	void display() {
---
+++ src/day08/CakeTest.java
@@ -0,0 +1,14 @@
+package day08;
+
+public class CakeTest {
+
+	public static void main(String[] args) {
+		Cake chocolate = new Cake();
+		chocolate.setName("巧克力蛋糕");
+		chocolate.setPrice(50);
+		chocolate.setSweetness(70.5);
+		chocolate.display();
+
+	}
+
+}
