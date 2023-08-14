745455500cf1dc8cdda15726735031d81683090f
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-14 13:58:14 +0800

    java

---
+++ src/day08/Drink.java
@@ -0,0 +1,15 @@
+package day08;
+
+// 飲料物件設計圖
+public class Drink { 
+	String name; // 飲料名稱 (物件變數)
+	int price;   // 飲料價格 (物件變數)
+	
+	void display() { // 顯示飲料名稱與價格的方法 (物件方法)
+		System.out.printf("名稱: %s 價格 $%d\n", name, price);
+	}
+	
+	void drink() { // 喝飲料的方法 (物件方法)
+		System.out.printf("喝 %s 的方法是打開蓋子就喝\n", name);
+	}
+}
