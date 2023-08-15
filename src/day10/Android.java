8fd8390500b2d1b0811c82517468800180e2529a
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-15 15:54:00 +0800

    java

--- src/day10/Android.java
+++ src/day10/Android.java
@@ -4,6 +4,26 @@ public class Android extends Mobile {
 	private boolean hasSD; // 是否有 SD 卡
 	private double androidVersion; // 8.1, 8.2
 	
+	public Android() {
+		
+	}
+	
+	public Android(String name, int storage, int price, boolean hasSD, double androidVersion) {
+		setName(name);
+		setStorage(storage);
+		setPrice(price);
+		setHasSD(hasSD);
+		setAndroidVersion(androidVersion);
+	}
+	
+	public void display() {
+		System.out.printf("機型: %s ", getName());
+		System.out.printf("容量: %d G ", getStorage());
+		System.out.printf("價格: $%d ", getPrice());
+		System.out.printf("SD: %b ", isHasSD());
+		System.out.printf("版本: %.1f\n", getAndroidVersion());
+	}
+	
 	public boolean isHasSD() {
 		return hasSD;
 	}
