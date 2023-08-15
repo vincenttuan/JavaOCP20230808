1f1e93b0d25e7def253a6b1bd9f0b8d8da1f98ac
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-15 10:10:10 +0800

    java

---
+++ src/day09/CoffeeTest.java
@@ -0,0 +1,18 @@
+package day09;
+
+public class CoffeeTest {
+	// Latte, Espresso, Cappuccino
+	// SMALL, MEDIUM, LARGE
+	// HOT, COLD
+	public static void main(String[] args) {
+		Coffee latte = new Coffee("Latte", "LARGE", "COLD");
+		latte.display();
+		
+		Coffee espresso = new Coffee("Espresso", "MEDIUM", "HOT");
+		espresso.display();
+		
+		Coffee mocha = new Coffee("Mocha", "SMALL", "COLD");
+		mocha.display();
+	}
+
+}
