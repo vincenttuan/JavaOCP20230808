2cfaab334b6eccb5dbca138b7aaf95e54825af25
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-15 13:21:53 +0800

    java

--- src/day09/Coffee.java
+++ src/day09/Coffee.java
@@ -38,6 +38,10 @@ public class Coffee {
 	}
 	
 	// 方法封裝 setters
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	public void setAmount(int amount) {
 		if(amount > 0) {
 			this.amount = amount;
