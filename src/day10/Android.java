cc24e12610055d3e5c5fa36472bc6f3bae923fa3
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-15 15:22:15 +0800

    java

---
+++ src/day10/Android.java
@@ -0,0 +1,20 @@
+package day10;
+
+public class Android {
+	private boolean hasSD; // 是否有 SD 卡
+	private double androidVersion; // 8.1, 8.2
+	
+	public boolean isHasSD() {
+		return hasSD;
+	}
+	public void setHasSD(boolean hasSD) {
+		this.hasSD = hasSD;
+	}
+	public double getAndroidVersion() {
+		return androidVersion;
+	}
+	public void setAndroidVersion(double androidVersion) {
+		this.androidVersion = androidVersion;
+	}
+	
+}
---
+++ src/day10/IPhone.java
@@ -0,0 +1,21 @@
+package day10;
+
+public class IPhone {
+	private boolean hasFaceId;
+	private int iosVersion; // 10, 11
+	
+	public boolean isHasFaceId() {
+		return hasFaceId;
+	}
+	public void setHasFaceId(boolean hasFaceId) {
+		this.hasFaceId = hasFaceId;
+	}
+	public int getIosVersion() {
+		return iosVersion;
+	}
+	public void setIosVersion(int iosVersion) {
+		this.iosVersion = iosVersion;
+	}
+	
+	
+}
