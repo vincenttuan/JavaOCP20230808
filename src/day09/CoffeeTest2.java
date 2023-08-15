d71fde183f598bed2f173bbad5cc7ed3c9e85c69
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-15 11:34:13 +0800

    java

--- src/day09/CoffeeTest2.java
+++ src/day09/CoffeeTest2.java
@@ -20,9 +20,14 @@ public class CoffeeTest2 {
 		// 2. 平均價
 		// 3. 大中小杯的數量
 		// 4. 冷熱的數量
+		// 5. 最昂貴的咖啡名稱與價格
+		// 6. 最便宜的咖啡名稱與價格
 		int totalSales = 0;
 		int smallCount = 0, mediumCount = 0, largeCount = 0;
 		int hotCount = 0, coldCount = 0;
+		Coffee mostExpensive = coffees[0];
+		Coffee cheapest = coffees[0];
+		
 		for(Coffee coffee : coffees) {
 			// 印出每一杯咖啡的價格
 			System.out.printf("%-12s $%3d\n", coffee.getName(), coffee.getPrice());
@@ -35,7 +40,7 @@ public class CoffeeTest2 {
 					break;
 				case "MEDIUM":
 					mediumCount++;
-					break;	
+					break;
 				case "SMALL":
 					smallCount++;
 					break;	
@@ -49,13 +54,25 @@ public class CoffeeTest2 {
 					coldCount++;
 					break;
 			}
+			// 更新最昂貴的咖啡
+			if(coffee.getPrice() > mostExpensive.getPrice()) {
+				mostExpensive = coffee;
+			}
+			// 更新最便宜的咖啡
+			if(coffee.getPrice() < cheapest.getPrice()) {
+				cheapest = coffee;
+			}
+			
 		}
 		System.out.println("-----------------");
 		System.out.printf("%-12s $%3d\n", "Total sales", totalSales);
 		System.out.printf("%-12s $%3d\n", "Average", totalSales/cups);
+		System.out.println("-----------------");
 		System.out.printf("L:%-2d M:%-2d S:%-2d\n", largeCount, mediumCount, smallCount);
 		System.out.printf("COLD:%-2d HOT:%-2d\n", coldCount, hotCount);
-		
+		System.out.println("----------------------------------");
+		System.out.printf("Most Expensive: %-12s $%3d\n", mostExpensive.getName(), mostExpensive.getPrice());
+		System.out.printf("Cheapest:       %-12s $%3d\n", cheapest.getName(), cheapest.getPrice());
 	}
 
 }
