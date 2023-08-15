1de46c34f5aa4d42bec3e5148dfd3c672e7ba37d
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-15 10:35:12 +0800

    java

--- src/day09/CoffeeTest2.java
+++ src/day09/CoffeeTest2.java
@@ -3,13 +3,13 @@ package day09;
 public class CoffeeTest2 {
 
 	public static void main(String[] args) {
-		Coffee latte = new Coffee("Latte", "LARGE", "COLD"); // 拿鐵
-		Coffee espresso = new Coffee("Espresso", "MEDIUM", "HOT"); // 濃縮
-		Coffee mocha = new Coffee("Mocha", "SMALL", "COLD"); // 摩卡
+		Coffee latte      = new Coffee("Latte", "LARGE", "COLD"); // 拿鐵
+		Coffee espresso   = new Coffee("Espresso", "MEDIUM", "HOT"); // 濃縮
+		Coffee mocha      = new Coffee("Mocha", "SMALL", "COLD"); // 摩卡
 		Coffee cappuccino = new Coffee("Cappuccino", "LARGE", "HOT"); // 卡布奇諾
-		Coffee americano = new Coffee("AmericanCoffee", "LARGE", "COLD"); // 美式
-		Coffee macchiate = new Coffee("Macchiate", "MEDIUM", "HOT"); // 瑪奇朵
-		Coffee flatWhite = new Coffee("FlatWhite", "SMALL", "HOT"); // 白咖啡
+		Coffee americano  = new Coffee("AmericanCoffee", "LARGE", "COLD"); // 美式
+		Coffee macchiate  = new Coffee("Macchiate", "MEDIUM", "HOT"); // 瑪奇朵
+		Coffee flatWhite  = new Coffee("FlatWhite", "SMALL", "HOT"); // 白咖啡
 		
 		Coffee[] coffees = {latte, espresso, mocha, cappuccino, americano, macchiate, flatWhite};
 		int cups = coffees.length;
