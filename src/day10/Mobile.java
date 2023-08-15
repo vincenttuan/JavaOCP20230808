7717dfb065247d6511c8291feb22f6176b2decf4
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-15 15:18:06 +0800

    java

---
+++ src/day10/Mobile.java
@@ -0,0 +1,30 @@
+package day10;
+
+public class Mobile {
+	
+	private String name;
+	private int storage;
+	private int price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStorage() {
+		return storage;
+	}
+	public void setStorage(int storage) {
+		this.storage = storage;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+}
