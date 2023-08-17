37096b3083fdb9424ff65611b3a43c356d24a48d
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 10:12:57 +0800

    java

---
+++ src/day11/EmpTest.java
@@ -0,0 +1,19 @@
+package day11;
+
+public class EmpTest {
+
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setSalary(40000);
+		System.out.printf("員工薪資 $%,d\n", emp.getSalary());
+		emp.job();
+		
+		
+		Manager mgr = new Manager();
+		mgr.setSalary(80000);
+		System.out.printf("經理薪資 $%,d\n", mgr.getSalary());
+		mgr.job();
+		
+	}
+
+}
