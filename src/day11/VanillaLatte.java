f846cf0fdba4d1c6a61336459dac5af7b7a3cd87
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 11:56:53 +0800

    java

--- src/day11/VanillaLatte.java
+++ src/day11/VanillaLatte.java
@@ -1,6 +1,15 @@
 package day11;
 
 public class VanillaLatte extends Latte {
- 
+
+	public VanillaLatte(String name, int price) {
+		super(name, price);
+	}
+
+	@Override
+	public void taste() {
+		System.out.printf("%s 帶有香草的香甜, 味道更加豐富\n", getName());
+	}
+	
 }
  
