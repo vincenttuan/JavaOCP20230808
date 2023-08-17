d783ab83b64f32acf35cade38b28ee0e073ec657
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 11:25:54 +0800

    java

--- src/day11/AnimalPolyTest.java
+++ src/day11/AnimalPolyTest.java
@@ -6,17 +6,19 @@ public class AnimalPolyTest {
 		// Animal 多型應用
 		Animal cat = new Cat();
 		Animal dog = new Dog();
+		Dog    dog2 = new Dog();
 		cat.sound();
 		dog.sound();
+		dog2.sound();
 		
-		Animal[] animals = {cat, dog};
+		Animal[] animals = {cat, dog, dog2};
 		for(Animal animal : animals) {
 			animal.sound();
 		}
 		
 		displayAnimalSound(cat);
 		displayAnimalSound(dog);
-		
+		displayAnimalSound(dog2);
 	}
 	
 	public static void displayAnimalSound(Animal animal) {
