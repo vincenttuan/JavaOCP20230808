2d89b3bde0a7080ec275a7a94c2d5558e7120888
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 11:49:33 +0800

    java

---
+++ src/day11/CoffeePloyTest.java
@@ -0,0 +1,13 @@
+package day11;
+
+public class CoffeePloyTest {
+
+	public static void main(String[] args) {
+		// Coffee 多型
+		Coffee coffee1 = new Espresso("濃縮咖啡", 70);
+		coffee1.taste();
+		System.out.printf("%s $%d\n", coffee1.getName(), coffee1.getPrice());
+		
+	}
+
+}
