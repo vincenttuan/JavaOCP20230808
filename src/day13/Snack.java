6dda5f79a932a899f794a5587ebd79e4efbc6947
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 15:36:42 +0800

    java

--- src/day13/Snack.java
+++ src/day13/Snack.java
@@ -5,6 +5,11 @@ package day13;
 // 具體類-OriginalChicken(原味) $50
 // 具體類-SpicyChicken(辣味風味) $55
 // 具體類-CheeseChicken(起司風味) $60
+
+// 抽象類-Tofu(豆腐) 預設價格 $40
+// 具體類-FiredTofu(炸豆腐) $ 預設價格 + 10
+// 具體類-PageTofu(百頁豆腐) $ 預設價格 + 15
+
 public interface Snack { // 小吃
 	void flavor(); // 風味
 	int getPrice(); // 取得價格
