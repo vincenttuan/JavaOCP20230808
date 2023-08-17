e19a69e65697fb4351a347b7385952be36fe871e
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 10:09:29 +0800

    java

---
+++ src/day11/Employee.java
@@ -0,0 +1,20 @@
+package day11;
+
+public class Employee {
+ 
+	private int salary;
+	 
+	public int getSalary() {
+		return salary;
+	}
+	 
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	 
+	public void job() {
+		System.out.println("員工的工作是:打雜");
+	}
+	 
+}
+ 
