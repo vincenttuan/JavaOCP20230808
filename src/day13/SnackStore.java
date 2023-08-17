06b0dfa34cdda42decc07edf6e6e6d334d6fd76b
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 15:55:37 +0800

    java

--- src/day13/SnackStore.java
+++ src/day13/SnackStore.java
@@ -7,15 +7,27 @@ public class SnackStore {
 		original.flavor();
 		System.out.printf("$%d\n", original.getPrice());
 		
+		FriedChicken cheese = new CheeseChicken(60);
+		cheese.flavor();
+		System.out.printf("$%d\n", cheese.getPrice());
+		
 		Tofu pageTofu = new PageTofu();
 		pageTofu.flavor();
 		System.out.printf("$%d\n", pageTofu.getPrice());
 		
-		Snack[] snacks = {original, pageTofu};
+		Snack[] snacks = {original, cheese, pageTofu}; // 所有小吃
 		for(Snack snack : snacks) {
 			snack.flavor();
 			System.out.printf("$%d\n", snack.getPrice());
 		}
+		
+		FriedChicken[] friedChickens = {original, cheese}; // 限定是鹽酥雞的小吃
+		for(FriedChicken friedChicken : friedChickens) {
+			friedChicken.flavor();
+			System.out.printf("$%d\n", friedChicken.getPrice());
+		}
+ 		
+		
 	}
 
 }
