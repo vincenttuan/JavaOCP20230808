91578a60df8fd0a4da18b14441e4de274ef2c2d1
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 11:51:45 +0800

    java

--- src/day11/Latte.java
+++ src/day11/Latte.java
@@ -1,6 +1,16 @@
 package day11;
 
 public class Latte extends Coffee {
- 
+
+	public Latte(String name, int price) {
+		super(name, price);
+	}
+
+	@Override
+	public void taste() {
+		System.out.printf("%s 味道柔和, 帶有濃厚的奶香\n", getName());
+	}
+	
+	
 }
  
