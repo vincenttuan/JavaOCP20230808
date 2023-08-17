8652daca7e88dd3f3deccb5b743f57e5cc0c6e2b
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 13:33:47 +0800

    java

--- src/day11/MatchaLatte.java
+++ src/day11/MatchaLatte.java
@@ -1,6 +1,15 @@
 package day11;
 
 public class MatchaLatte extends Latte {
- 
+
+	public MatchaLatte(String name, int price) {
+		super(name, price);
+	}
+
+	@Override
+	public void taste() {
+		System.out.printf("%s 味道清新，帶有抹茶的獨特風味。\n", getName());
+	}
+	
 }
  
