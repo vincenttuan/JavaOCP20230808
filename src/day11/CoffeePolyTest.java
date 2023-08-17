85007dd023e4b9d65d7f2b8d10d31b1c254a3853
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 11:53:45 +0800

    java

--- src/day11/CoffeePolyTest.java
+++ src/day11/CoffeePolyTest.java
@@ -5,9 +5,18 @@ public class CoffeePolyTest {
 	public static void main(String[] args) {
 		// Coffee 多型
 		Coffee coffee1 = new Espresso("濃縮咖啡", 70);
+		Coffee coffee2 = new Latte("拿鐵", 60);
+		
 		coffee1.taste();
 		System.out.printf("%s $%d\n", coffee1.getName(), coffee1.getPrice());
 		
+		coffee2.taste();
+		System.out.printf("%s $%d\n", coffee2.getName(), coffee2.getPrice());
+		
+		Coffee[] coffees = {coffee1, coffee2};
+		for(Coffee coffee : coffees) {
+			coffee.taste();
+		}
 	}
 
 }
