fedc4f30684dad5c37da63413ca51c625bbc71ab
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 11:46:54 +0800

    java

--- src/day11/Espresso.java
+++ src/day11/Espresso.java
@@ -5,7 +5,11 @@ public class Espresso extends Coffee {
 	public Espresso(String name, int price) {
 		super(name, price);
 	}
-	
+
+	@Override
+	public void taste() {
+		System.out.printf("%s 味道濃郁, 帶有深培的風味\n", getName());
+	}
 	
 	
 }
