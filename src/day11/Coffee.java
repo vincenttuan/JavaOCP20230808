fffa49227ea9c9bc42e2986e805a1cb85cdc638e
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-17 11:45:21 +0800

    java

--- src/day11/Coffee.java
+++ src/day11/Coffee.java
@@ -10,10 +10,30 @@ public class Coffee {
 	}
 	 
 	public Coffee(String name, int price) {
+		this.name = name;
+		this.price = price;
 	}
 	 
 	public void taste() {
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
 	 
 }
  
--- src/day11/Espresso.java
+++ src/day11/Espresso.java
@@ -1,6 +1,12 @@
 package day11;
 
 public class Espresso extends Coffee {
- 
+
+	public Espresso(String name, int price) {
+		super(name, price);
+	}
+	
+	
+	
 }
  
