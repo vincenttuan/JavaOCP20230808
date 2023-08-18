0e9848081e2a7f7c56a5be25c3610005a0549612
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-18 15:32:46 +0800

    java

---
+++ src/stream/StreamDemo3.java
@@ -0,0 +1,12 @@
+package stream;
+
+public class StreamDemo3 {
+
+	public static void main(String[] args) {
+		// 傳統陣列 (車速)
+		int[] speeds = {100, 42, 83, 70, -30, 55, 203};
+		// 限速是 70 公里, 每超過一公里罰款 100 元, 請印出每台車的罰款金額, 若無罰款則顯示 0
+		
+	}
+
+}
