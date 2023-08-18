c04313c1d2478988ac30ab4c388adf1dab60aa22
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-18 09:52:33 +0800

    java

---
+++ src/day14/CImpl.java
@@ -0,0 +1,16 @@
+package day14;
+
+public class CImpl implements C {
+	@Override
+	public void foo() {
+		System.out.println("foo");
+	}
+	@Override
+	public void bar() {
+		System.out.println("bar");
+	}
+	@Override
+	public void demo() {
+		System.out.println("demo");
+	}
+}
