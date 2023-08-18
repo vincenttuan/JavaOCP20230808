c4e7a7ea442a7c6fb0bdfee105c8d8d33b8eb83f
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-18 13:33:22 +0800

    java

--- src/stream/StreamDemo1.java
+++ src/stream/StreamDemo1.java
@@ -20,7 +20,12 @@ public class StreamDemo1 {
 				  }
 			  });
 		
-
+		// 進行串流轉換並先將元素透過中間流filter來過濾出及格的元素, 最後透過終點流 forEach 將每一個元素印出
+		Arrays.stream(scores)  // 1.進行串流轉換
+			  .filter(score -> score >= 60)  // 2.透過中間流filter來過濾出及格的元素
+			  .forEach(score -> System.out.println(score));  // 3.終點流 forEach 將每一個元素印出 
+		
+		
 	}
 
 }
