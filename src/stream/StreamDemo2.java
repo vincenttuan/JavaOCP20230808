08d7438a0cd6bdced649a3e2302a12ce09c9f1f8
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-18 13:59:29 +0800

    java

---
+++ src/stream/StreamDemo2.java
@@ -0,0 +1,14 @@
+package stream;
+
+public class StreamDemo2 {
+
+	public static void main(String[] args) {
+		// 傳統陣列
+		int[] scores = {100, 42, 83, 70, -30, 55, 203};
+		// 合法分數範圍是 0~100
+		// 請利用 Stream 語法印出合法的分數
+		
+
+	}
+
+}
