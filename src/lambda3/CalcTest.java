df47b94769140a9eb2b91dbe389c275098d9f076
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-18 11:26:52 +0800

    java

--- src/lambda3/CalcTest.java
+++ src/lambda3/CalcTest.java
@@ -1,7 +1,11 @@
 package lambda3;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class CalcTest {
 
@@ -15,6 +19,17 @@ public class CalcTest {
 		Consumer<String> welcome = name -> System.out.printf("歡迎 %s 蒞臨本公司指導!\n", name);
 		welcome.accept("Vincent");
 		
+		// Predicate 的使用
+		Predicate<Integer> scorePass = score -> score >= 60;
+		System.out.println(scorePass.test(75));
+		System.out.println(scorePass.test(45));
+		
+		// Supplier 的使用
+		Supplier<Date> today = () -> new Date();
+		System.out.println(today.get());
+		
+		Supplier<Integer> luckyNumber = () -> new Random().nextInt(100);
+		System.out.println(luckyNumber.get());
 		
 	}
 
