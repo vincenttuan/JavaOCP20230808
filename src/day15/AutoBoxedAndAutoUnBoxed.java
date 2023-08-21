571090d5b1a1c050144f672c0d0b058af3a0b2e9
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-21 10:13:05 +0800

    java

--- src/day15/AutoBoxedAndAutoUnBoxed.java
+++ src/day15/AutoBoxedAndAutoUnBoxed.java
@@ -1,5 +1,8 @@
 package day15;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 // 自動裝箱(auto-boxed)與自動拆箱(auto-unboxed)
 // Java 5.0(含)版之後提供
 public class AutoBoxedAndAutoUnBoxed {
@@ -16,6 +19,19 @@ public class AutoBoxedAndAutoUnBoxed {
 		Integer y = 200; // 編譯器會自動轉成 Integer.valueOf(200), 這個過程叫做:自動裝箱(auto-boxed)
 		int total = x + y; // 編譯器針對 y 會自動轉成 y.intValue(), 這個過程叫做:自動拆箱(auto-unboxed)
 		System.out.printf("x=%d, y=%d, total=%d\n", x, y, total);
+		
+		// boxed() 使用範例
+		int[] scores = {90, 100, 80};
+		// sorted() 自然排序(小->大)
+		Arrays.stream(scores).sorted().forEach(System.out::println);
+		// sorted() 反排序(大->小)
+		Arrays.stream(scores) // int 串流
+			  .boxed()  // 進行裝箱變成 Integer 串流 
+			  .sorted(Comparator.reverseOrder()) // 已經反序的 Integer 串流
+			  //.mapToInt(n -> n.intValue()) // 再轉為 int 串流 (此行可以省略)
+			  //.mapToInt(n -> n) // 相當於 mapToInt(n -> n.intValue()) 自動拆箱 (此行可以省略)
+			  .mapToInt(Integer::intValue) // 透過方法參考 (此行可以省略)
+			  .forEach(System.out::println);
 	}
 
 }
