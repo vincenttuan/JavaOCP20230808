e17e0fa11f35eecf29cbcfec02259eced54ff1e2
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-21 13:15:11 +0800

    java

--- src/day15/AnonymousInnerClassTest.java
+++ src/day15/AnonymousInnerClassTest.java
@@ -4,6 +4,9 @@ public class AnonymousInnerClassTest {
 
 	public static void main(String[] args) {
 		Pet pet = new Pet() {
+			{
+				System.out.println("Pet");
+			}
 			public void skill() {
 				System.out.println("玩飛盤");
 			}
@@ -13,6 +16,9 @@ public class AnonymousInnerClassTest {
 		} ;
 		
 		Pet pet2 = new Pet() {
+			{
+				System.out.println("Pet2");
+			}
 			public void skill() {
 				System.out.println("玩毛線");
 			}
