04ad068012d8761170af8277606ea974eefd4846
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-21 10:25:11 +0800

    java

--- src/day15/InnerClassDemo.java
+++ src/day15/InnerClassDemo.java
@@ -3,8 +3,15 @@ package day15;
 public class InnerClassDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 1. 建立 Outer 物件
+		Outer outer = new Outer();
+		// 2. 透過 outer 建立 Inner 物件
+		Outer.Inner inner = outer.new Inner();
+		// 3. 使用 inner 所提供的資源
+		inner.callMe();
+		
+		// 簡便寫法
+		new Outer().new Inner().callMe();
 	}
 
 }
