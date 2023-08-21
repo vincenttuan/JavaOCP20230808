81bf83d2612d6667fbc0177fc7db2a5c92d2c824
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-21 10:29:30 +0800

    java

--- src/day15/Outer.java
+++ src/day15/Outer.java
@@ -1,11 +1,18 @@
 package day15;
 
 public class Outer {
+	int x = 7;
 	
 	public class Inner {
+		int x = 77;
 		
 		public void callMe() {
+			int x = 777;
+					
 			System.out.println("Hello Inner...");
+			System.out.println(x); // 777
+			System.out.println(this.x); // 77
+			System.out.println(Outer.this.x); // 7
 		}
 		
 	}
