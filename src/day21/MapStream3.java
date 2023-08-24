b9fb391e53a6784c98069d566f1978d897ec56d5
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-24 13:33:02 +0800

    java

---
+++ src/day21/MapStream3.java
@@ -0,0 +1,16 @@
+package day21;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapStream3 {
+
+	public static void main(String[] args) {
+		// 學生姓名如下:
+		List<String> names = 
+				Arrays.asList("王大明", "李曉華", "張三", "李四", "王五", "趙六", "王子麵", "張暖暖", "趙框應", "王靜");
+		// 學生姓名請按照姓氏分組
+
+	}
+
+}
