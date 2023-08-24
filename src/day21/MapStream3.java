b5c6f3af4fe873aa8c30a646829ff2455b2a5215
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-24 13:39:43 +0800

    java

--- src/day21/MapStream3.java
+++ src/day21/MapStream3.java
@@ -2,6 +2,9 @@ package day21;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.counting;
 
 public class MapStream3 {
 
@@ -12,6 +15,10 @@ public class MapStream3 {
 		// 學生姓名請按照姓氏分組
 		// 提示: "王大明".charAt(0) 可以得到姓氏
 		// 提示: name -> name.charAt(0) 可以得到姓氏
+		
+		Map<Character, Long> result = names.stream()
+				.collect(groupingBy(name -> name.charAt(0), counting()));
+		System.out.println(result);
 		
 	}
 
