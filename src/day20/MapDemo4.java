29c613d811a39d87cca68f08e2cd9daa3c3a5133
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-24 11:24:49 +0800

    java

---
+++ src/day20/MapDemo4.java
@@ -0,0 +1,13 @@
+package day20;
+
+public class MapDemo4 {
+	public static void main(String[] args) {
+		// 每一個員工與薪資的關係
+		// {John=50000, Mary=60000, Steve=55000, Anna=57000}
+		// 每一個員工與薪資上調比例的關係
+		// {John=0.10, Mary=0.07, Steve=0.04, Anna=0.06}
+		// 請利用 Map 計算每一個員工調整後的薪資
+		
+		
+	}
+}
