ce1b0d4cc765a0f6b562a36f1c48ed81aff47e7f
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-24 11:53:26 +0800

    java

--- src/day20/MapDemo5.java
+++ src/day20/MapDemo5.java
@@ -1,5 +1,9 @@
 package day20;
 
+import java.util.HashMap;
+import java.util.Map;
+import static java.util.Map.Entry;
+
 public class MapDemo5 {
 	/*
 	 * 你和你的室友每個月都會花費在一些共同的開銷，如電費、網費、租金等。
@@ -7,7 +11,23 @@ public class MapDemo5 {
 	 * 為了追踪每人所支付的總金額和每項費用的總金額，你們決定使用 Map 來記錄。
 	 * */
 	public static void main(String[] args) {
+		Map<String, Integer> map1 = new HashMap<>() {{
+			put("租金", 3000);
+			put("電費", 500);
+		}};
 		
+		Map<String, Integer> map2 = new HashMap<>() {{
+			put("租金", 3000);
+			put("網費", 1000);
+			put("電費", 400);
+		}};
+		
+		// 將 map1 與 map2 合併(merge)
+		// 例如: {租金=6000, 網費=1000, 電費=900}
+		for(Entry<String, Integer> entry : map2.entrySet()) {
+			map1.merge(entry.getKey(), entry.getValue(), (a, b) -> a + b);
+		}
+		System.out.println(map1);
 
 	}
 
