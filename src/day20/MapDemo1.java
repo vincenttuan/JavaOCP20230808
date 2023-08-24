bc5892f22b3d6dbe197a7a91910a389d207ae091
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-24 09:55:15 +0800

    java

--- src/day20/MapDemo1.java
+++ src/day20/MapDemo1.java
@@ -18,10 +18,21 @@ public class MapDemo1 {
 		// 2. 向 HashMap 添加鍵值對
 		map.put("John", 25);
 		map.put("Mary", 30);
+		map.put("Jack", 28);
+		map.put("Rose", 26);
 		System.out.println(map);
 		// 3. 查詢 john(鍵) 的年齡(值)
 		Integer age = map.get("John");
 		System.out.println(age);
+		// 4. 檢查某個鍵是否存在於 Map 中
+		boolean exists = map.containsKey("John");
+		System.out.println(exists);
+		// 5. 檢查某個值是否存在於 Map 中
+		exists = map.containsValue(30);
+		System.out.println(exists);
+		// 6. 移除一個鍵值對
+		map.remove("Mary"); // 放入 key
+		System.out.println(map);
 	}
 
 }
