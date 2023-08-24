423f60b5c3a8ff534fae74976bc18b75bdf945b5
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-24 10:30:34 +0800

    java

---
+++ src/day20/MapDemo3.java
@@ -0,0 +1,17 @@
+package day20;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapDemo3 {
+
+	public static void main(String[] args) {
+		// 學生成績的集合
+		List<Integer> scores = 
+				Arrays.asList(85, 90, 75, 90, 80, 85, 70, 90, 80);
+		// 每一個成績的學生數量
+		// 例如: {70=1, 75=1, 80=2, 85=3, 90=3}
+		
+	}
+
+}
