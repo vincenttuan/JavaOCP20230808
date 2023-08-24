014cdae806de080e3a98bc17f722e439b3794d54
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-24 11:16:59 +0800

    java

--- src/day20/MapDemo3.java
+++ src/day20/MapDemo3.java
@@ -1,7 +1,9 @@
 package day20;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MapDemo3 {
 
@@ -11,7 +13,17 @@ public class MapDemo3 {
 				Arrays.asList(85, 90, 75, 90, 80, 85, 70, 90, 80);
 		// 每一個成績的學生數量
 		// 例如: {70=1, 75=1, 80=2, 85=3, 90=3}
+		Map<Integer, Integer> result = new HashMap<>();
 		
+		for(Integer score : scores) {
+			if(result.containsKey(score)) {
+				result.put(score, result.get(score) + 1);
+			} else {
+				result.put(score, 1);
+			}
+		}
+		System.out.println(result);
+		System.out.println(result.get(90));
 	}
 
 }
