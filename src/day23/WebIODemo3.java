53651e84df2e30dc86e5e0186323466ebc446bbb
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-25 15:29:36 +0800

    java

--- src/day23/WebIODemo3.java
+++ src/day23/WebIODemo3.java
@@ -7,13 +7,12 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
 
-public class WebIODemo2 {
+public class WebIODemo3 {
 	// 利用 Java 11 提供的網路 API 套件 HttpClient 與 HttpRequest 來訪問指定 URL
-	// (編碼 Big5)
+	// 市售米抽檢不合格 (編碼 UTF-8)
 	public static void main(String[] args) {
 		// 1. 建立 http 網路連線位置(網址)
-		String date = "20230824";
-		String urlString = "https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=" + date + "&selectType=ALL";
+		String urlString = "https://data.moa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx";
 		
 		// 2. 建立 HttpClient
 		HttpClient client = HttpClient.newHttpClient();
@@ -26,7 +25,7 @@ public class WebIODemo2 {
 		
 		// 4. 執行請求(request)並接收回應(response)
 		try {
-			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString(Charset.forName("Big5")));
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString(Charset.forName("UTF-8")));
 			// 5. 印出回應結果
 			System.out.println(response.body());
 		} catch (IOException | InterruptedException e) {
