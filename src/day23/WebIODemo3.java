54a704058fda99194ac6f5516161e557359e654b
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-25 15:34:07 +0800

    java

--- src/day23/WebIODemo3.java
+++ src/day23/WebIODemo3.java
@@ -7,6 +7,8 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
 
+import com.google.gson.Gson;
+
 public class WebIODemo3 {
 	// 利用 Java 11 提供的網路 API 套件 HttpClient 與 HttpRequest 來訪問指定 URL
 	// 市售米抽檢不合格 (編碼 UTF-8)
@@ -28,7 +30,11 @@ public class WebIODemo3 {
 			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString(Charset.forName("UTF-8")));
 			// 5. 回應結果: 得到 json String
 			String jsonString = response.body();
-			System.out.println(jsonString);
+			//System.out.println(jsonString);
+			// 6. 利用 Gson 將 json 陣列字串轉 BadRice[]
+			Gson gson = new Gson();
+			BadRice[] badRices = gson.fromJson(jsonString, BadRice[].class);
+			System.out.printf("資料筆數: %d\n", badRices.length);
 			
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
