26c0fe7cf4c30be6b5d075c990d3aacded5316e2
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-25 14:27:07 +0800

    java

--- src/day23/WebIODemo1.java
+++ src/day23/WebIODemo1.java
@@ -6,9 +6,39 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class WebIODemo1 {
+	static {
+        // 忽略 SSL 憑證驗證
+        TrustManager[] trustAllCertificates = new TrustManager[]{
+            new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
 
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCertificates, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
 	public static void main(String[] args) {
 		// 1. 建立 http 網路連線位置(網址)
 		String date = "20230824";
