ac2fd6a0e590252dfc6ba5b9402b9f36df885351
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-25 10:08:43 +0800

    java

--- src/day22/ExceptionDemo1.java
+++ src/day22/ExceptionDemo1.java
@@ -2,6 +2,7 @@ package day22;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 // 密碼:1234 (數字)
 // 1. 輸入密碼 -> 成功(2) -> 失敗(1)
 // 2. 輸入年齡 -> 得到幸運數字
@@ -16,14 +17,21 @@ public class ExceptionDemo1 {
 	public static void login() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("請輸入四位數密碼:");
-		int userPwd = sc.nextInt();
-		if(userPwd == 1234) {
-			System.out.println("登入成功");
-			play();
-		} else {
-			System.out.println("登入失敗, 請重新登入!");
+		
+		try {
+			int userPwd = sc.nextInt();
+			if(userPwd == 1234) {
+				System.out.println("登入成功");
+				play();
+			} else { // 密碼錯誤得失敗
+				System.out.println("登入失敗, 請重新登入!");
+				login(); // 重新呼叫 login()
+			}
+		} catch(InputMismatchException e) { // 輸入的型別不正確的失敗
+			System.out.printf("登入錯誤: %s 請重新登入!\n", e.toString());
 			login(); // 重新呼叫 login()
 		}
+		
 	}
 	
 	// 遊戲開始
