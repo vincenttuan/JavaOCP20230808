7507d076dbed711d9b82677cbfef69187e537518
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-25 14:05:00 +0800

    java

--- src/day23/WebIODemo1.java
+++ src/day23/WebIODemo1.java
@@ -3,7 +3,9 @@ package day23;
 public class WebIODemo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String date = "20230824";
+		String urlString = "https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=" + date + "&selectType=ALL";
+		
 
 	}
 
