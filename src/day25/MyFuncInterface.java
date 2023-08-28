25b9f6bf43b0c333e3c37732facad97c630291b4
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-28 13:11:27 +0800

    java

---
+++ src/day25/MyFuncInterface.java
@@ -0,0 +1,23 @@
+package day25;
+
+@FunctionalInterface
+public interface MyFuncInterface {
+	
+	boolean equals(Object obj); // Object 的方法
+	String toString(); // Object 的方法
+	void foo();
+	
+	// Java 8 提供 default 方法來做為介面實作
+	default void copyright() {
+		System.out.println("copyright ..." + getVersion());
+	}
+	
+	// Java 11 提供了可以給 default 方法用的 private 方法
+	private double getVersion() {
+		return 2.0;
+	}
+	
+	static void print() {
+		System.out.println("print");
+	}
+}
