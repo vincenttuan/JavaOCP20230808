1ca3f911d8c942ace74badcb04c9879ba3a6812f
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-28 13:50:05 +0800

    java

--- src/day25/BankAccount.java
+++ src/day25/BankAccount.java
@@ -8,18 +8,23 @@ public class BankAccount {
 	}
 	
 	// 提款方法
-	public void withdraw(int amount) {
+	public synchronized void withdraw(int amount) {
 		String threadName = Thread.currentThread().getName();
 		System.out.printf("%s 要提款 $%d\n", threadName, amount);
 		
 		// 判斷帳戶餘額
 		if(amount > 0 && amount <= balance) {
+			
+			for(int i=0;i<=100000000;i++); // 模擬花費時間
+			
 			System.out.printf("%s 提款 $%d 成功\n", threadName, amount);
-			balance =- amount; // 減去餘額
+			balance -= amount; // 減去餘額
 		} else {
 			System.out.printf("%s 提款 $%d 失敗\n", threadName, amount);
 		}
 		
+		// 印出目前餘額
+		System.out.printf("%s 查看目前餘額 $%d\n", threadName, balance);
 	}
 	
 }
