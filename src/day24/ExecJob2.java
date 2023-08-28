4ed4d5c4028a346b2e0b2f70a5941867fbfb647d
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-28 10:13:27 +0800

    java

--- src/day24/ExecJob2.java
+++ src/day24/ExecJob2.java
@@ -9,12 +9,18 @@ public class ExecJob2 {
 		Runnable even = new Even();
 		
 		// 2.建立執行緒物件並賦予工作
-		Thread t1 = new Thread(odd);
+		// 預設的執行緒是 UserThread
+		Thread t1 = new Thread(odd); // 建立一個空的執行緒並賦予 odd 工作
 		Thread t2 = new Thread(even);
+		Thread t3 = new JobThread();
 		
 		// 下達 start() 指令開始作業
 		t1.start();
 		t2.start();
+		t3.start();
+		
+		// 計算總共有幾條活著的執行緒
+		System.out.printf("Active 執行緒數量: %d\n", Thread.activeCount());
 		
 	}
 }
