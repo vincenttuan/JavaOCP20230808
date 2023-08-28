c39f784f321846c50820670f983120fe4edae9ee
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-28 15:41:00 +0800

    java

--- src/day25/StreamParallelExample.java
+++ src/day25/StreamParallelExample.java
@@ -5,22 +5,23 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 // 執行緒平行計算
+
 public class StreamParallelExample {
 
 	public static void main(String[] args) {
 		//List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-		List<Integer> numbers = IntStream.rangeClosed(1, 10_0000)
+		List<Integer> numbers = IntStream.rangeClosed(1, 100_0000)
 										 .boxed()
 										 .collect(Collectors.toList());
 		
 		// 計時開始
 		long startTime = System.nanoTime();
 		
-		// 計算每個元素平方之後的總和
+		// 計算 complexCalculation
 		// 利用 parallelStream() 平行串流
-		int sum = numbers.parallelStream()
-						 .mapToInt(num -> (num * num + num * num * num) / 2)
-                		 .sum();
+		long sum = numbers.parallelStream()
+					.mapToLong(StreamParallelExample::complexCalculation)
+					.sum();
 		
 		// 計時開始
 		long endTime = System.nanoTime();
@@ -28,7 +29,13 @@ public class StreamParallelExample {
 		System.out.println(sum);
 		System.out.printf("花費時間: %,d\n", (endTime - startTime));
 		
-
 	}
+	
+	// 增加計算複雜度的方法
+    private static long complexCalculation(int num) {
+        return IntStream.rangeClosed(1, 100)
+                        .mapToLong(i -> i * num)
+                        .sum();
+    }
 
 }
