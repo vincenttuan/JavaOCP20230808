6e321b7d357b0ecee233e2e061a065d23c618a0f
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-28 14:21:45 +0800

    java

---
+++ src/day25/Car.java
@@ -0,0 +1,31 @@
+package day25;
+
+import java.util.Random;
+import java.util.concurrent.CyclicBarrier;
+
+public class Car extends Thread {
+	private CyclicBarrier cyclicBarrier;
+	
+	public Car(CyclicBarrier cyclicBarrier) {
+		this.cyclicBarrier = cyclicBarrier;
+	}
+	
+	@Override
+	public void run() {
+		String threadName = Thread.currentThread().getName();
+		System.out.printf("%s 出發了\n", threadName);
+		
+		try {
+			Thread.sleep(new Random().nextInt(5000)); // 模擬做工的時間
+			System.out.printf("%s 到苗栗了\n", threadName);
+			cyclicBarrier.await(); // 等待其他人
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		System.out.printf("%s 結束工作\n", threadName);
+	}
+	
+}
