3f3390b94fcb458b9a9410600415d1632543176e
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-29 15:11:03 +0800

    java

--- src/day27/CreateSaleRecords.java
+++ src/day27/CreateSaleRecords.java
@@ -14,11 +14,11 @@ public class CreateSaleRecords {
 			PreparedStatement pstmt = conn.prepareStatement(sql);) {
 			
 			pstmt.setString(1, "2023-08-29");
-			pstmt.setString(2, "咖啡");
-			pstmt.setDouble(3, 65.0);
-			pstmt.setInt(4, 15);
+			pstmt.setString(2, "珍珠奶茶");
+			pstmt.setDouble(3, 45.0);
+			pstmt.setInt(4, 12);
 			pstmt.setString(5, "桃園");
-			pstmt.setString(6, "中山門市");
+			pstmt.setString(6, "中華門市");
 			
 			int rowcount = pstmt.executeUpdate(); // rowcount 異動筆數
 			System.out.printf("新增筆數: %d\n", rowcount);
