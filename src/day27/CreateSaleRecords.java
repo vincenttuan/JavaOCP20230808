e6a7bf8563d0176f3875ea9e8ec07037e04edc3c
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-29 15:03:31 +0800

    java

---
+++ src/day27/CreateSaleRecords.java
@@ -0,0 +1,11 @@
+package day27;
+
+public class CreateSaleRecords {
+
+	public static void main(String[] args) {
+		// 新增紀錄
+		String sql = "insert into salerecords(date, product, price, quantity, city, branch) value(?, ?, ?, ?, ? ?)";
+
+	}
+
+}
