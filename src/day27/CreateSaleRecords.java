ead8dbcc5aa965a3cd8f501ffbb3df8e89548945
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-29 15:09:23 +0800

    java

--- src/day27/CreateSaleRecords.java
+++ src/day27/CreateSaleRecords.java
@@ -1,11 +1,32 @@
 package day27;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CreateSaleRecords {
 
 	public static void main(String[] args) {
 		// 新增紀錄
-		String sql = "insert into salerecords(date, product, price, quantity, city, branch) value(?, ?, ?, ?, ? ?)";
-
+		String sql = "insert into salerecords(date, product, price, quantity, city, branch) value(?, ?, ?, ?, ?, ?)";
+		
+		try(Connection conn = MySQL.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql);) {
+			
+			pstmt.setString(1, "2023-08-29");
+			pstmt.setString(2, "咖啡");
+			pstmt.setDouble(3, 65.0);
+			pstmt.setInt(4, 15);
+			pstmt.setString(5, "桃園");
+			pstmt.setString(6, "中山門市");
+			
+			int rowcount = pstmt.executeUpdate(); // rowcount 異動筆數
+			System.out.printf("新增筆數: %d\n", rowcount);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
