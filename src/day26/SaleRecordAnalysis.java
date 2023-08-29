64624a302c6729f9e0472b38468d258408bf1eec
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-29 11:46:22 +0800

    java

--- src/day26/SaleRecordAnalysis.java
+++ src/day26/SaleRecordAnalysis.java
@@ -1,7 +1,13 @@
 package day26;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toSet;
 
 public class SaleRecordAnalysis {
 
@@ -30,7 +36,40 @@ public class SaleRecordAnalysis {
 									.collect(toList());
 		System.out.println(branchs);
 		
+		// 每一個城市擁有那些分店 Map<String, Set<String>>
+		Map<String, Set<String>> cityToBranches = null;
+		cityToBranches = records.stream()
+								.collect(groupingBy(
+									SaleRecord::getCity, // 按城市分組	
+									mapping(SaleRecord::getBranch, toSet())	
+								));
+		
+		cityToBranches.forEach((k, v) -> System.out.printf("%s: %s\n", k, v));
+		
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
