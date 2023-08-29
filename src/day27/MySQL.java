4efd866a9d358613ab05aa722a9cda7b7772ff04
MB-teacher <MB-teacher@MB-207-teacher>
2023-08-29 15:01:18 +0800

    java

--- src/day27/MySQL.java
+++ src/day27/MySQL.java
@@ -8,6 +8,8 @@ public class MySQL {
 	
 	static {
 		try {
+			// JDBC 3.0(含)以前需要寫
+			// JDBC 4.0(含)以上不用寫 jar 包中有看到 /META-INF/services/java.sql.Driver
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			String dbURL = "jdbc:mysql://localhost:3306/demo?zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true";
 			String username = "root";
